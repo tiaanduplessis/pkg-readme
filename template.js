479bf3d655bbd875bf8d43298ea4d671a6526ff6
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-02-11 14:39:56 +0200

    Update template

--- template.js
+++ template.js
@@ -7,7 +7,7 @@ $
 
 `
 
-module.exports = function template (opts = {name: '', license: '', description: '', example: '', bin: false}) {
+module.exports = function template (opts = {name: '', license: '', description: '', example: '', bin: false, author: '', repository: ''}) {
   return `
 # ${opts.name}
 [![package version](https://img.shields.io/npm/v/${opts.name}.svg?style=flat-square)](https://npmjs.org/package/${opts.name})
@@ -50,6 +50,6 @@ ${opts.bin ? cliTemplate : ''}
 
 ## License
 
-${opts.license}
+${opts.license} ${opts.author && opts.repository ? `© [${opts.author}](${opts.repository})` : ''}
     `
 }
